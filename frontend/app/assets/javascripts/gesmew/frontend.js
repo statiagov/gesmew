6ac7f979468544fc4406c7a71ac2bb6dda029870
Michail Gumbs <j4m4l@Michails-MacBook-Air.local>
2015-08-06 12:16:38 -0400

    changes

--- frontend/app/assets/javascripts/gesmew/frontend.js
+++ frontend/app/assets/javascripts/gesmew/frontend.js
@@ -1,6 +1,5 @@
 //= require bootstrap-sprockets
 //= require jquery.validate/jquery.validate.min
 //= require gesmew
-//= require gesmew/frontend/cart
 //= require gesmew/frontend/checkout
 //= require gesmew/frontend/product
